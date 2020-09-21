ea07309e594cc5aea58ddedbf3b01f562d16c9e5
grilo13 <56405847+grilo13@users.noreply.github.com>
2020-09-21 03:02:29 +0100

    Horizontal movement added to the snake.
    Added also is velocity. Now moving to the bounds of the square.

--- codigo_jogo.js
+++ codigo_jogo.js
@@ -3,14 +3,32 @@ const background = document.getElementById("Mycanvas");
 const snake_ctx = background.getContext("2d");
 
 // Posição inicial da cobra, tamanho = 5
-let snake = [  {x: 200, y: 200},  {x: 190, y: 200},  
-               {x: 180, y: 200},  {x: 170, y: 200},  {x: 160, y: 200},];
+let snake = [ {x: 200, y: 200},  
+              {x: 190, y: 200},  
+              {x: 180, y: 200},  
+              {x: 170, y: 200},  
+              {x: 160, y: 200},];
+
+// Velocidade horizontal
+let dx = 10;
+// Velocidade vertical
+let dy = 0;
 
 main();
 
+//A funcao main repete-se e por isso a setTimeout é chamada vezes infinitas
+
 function main() {
+
+  setTimeout(function onTick(){
+
   clearCanvas();
+  move_snake();
   drawSnake();
+
+  main();
+
+  }, 100)
 }
 
 // Função para desenhar a cobra em si
@@ -36,5 +54,16 @@ function drawSnake() {
   snake_ctx.fillRect(0, 0, background.width, background.height);
   // Draw a "border" around the entire canvas
   snake_ctx.strokeRect(0, 0, background.width, background.height);
+}
+
+// Função que movimenta a cobra - tira a parte de trás e adiciona na parte de frente, e assim por diante
+// Dx simboliza a velocidade horizontal e dy simboliza a velocidade vertical
+
+function move_snake() {
+  // Create the new Snake's head
+  const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+  // Add the new head to the beginning of snake body
+  snake.unshift(head);
+  snake.pop();
 }
 
